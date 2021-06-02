a33b24dd959cc11ecf52e923915e293d0b9403e0
Shradha1824 <shradhabj1824@gmail.com>
2021-06-01 14:42:20 -1200

    First commit

---
+++ TicTocToe.java
@@ -0,0 +1,16 @@
+public class CreateBoardUC1 {
+
+        public static void main(String[] args) {
+
+                char[][] board = {{' ', ' ', ' '},
+                                  {' ', ' ', ' '},
+                                  {' ', ' ', ' '}};
+
+                System.out.println(board[0][0] + "|" + board[0][1] + "|" + boar>
+                System.out.println("-+-+-");
+                System.out.println(board[1][0] + "|" + board[1][1] + "|" + boar>
+                System.out.println("-+-+-");
+                System.out.println(board[2][0] + "|" + board[2][1] + "|" + boar>
+                
+		}
+           }
