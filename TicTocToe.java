c3c4ffca3e7073d3cb307e6890e6d62f960f1b68
Shradha1824 <shradhabj1824@gmail.com>
2021-06-01 14:47:33 -1200

    Added file

--- TicTocToe.java
+++ TicTocToe.java
@@ -1,4 +1,4 @@
-public class CreateBoardUC1 {
+public class TicTocToe {
 
         public static void main(String[] args) {
 
@@ -12,5 +12,55 @@ public class CreateBoardUC1 {
                 System.out.println("-+-+-");
                 System.out.println(board[2][0] + "|" + board[2][1] + "|" + boar>
                 
+                 Scanner scanner = new Scanner(System.in);
+
+        //      System.out.println("Where would you like to play? (1-9) ");
+
+                String userInput = scanner.nextLine();
+
+                System.out.println(userInput);
+
+                switch(userInput) {
+                case "1":
+                        board[0][0] = 'X';
+                        break;
+                case "2":
+                        board[0][1] = 'X';
+                        break;
+                case "3":
+                        board[0][2] = 'X';
+                        break;
+                case "4":
+                        board[1][0] = 'X';
+                        break;
+                case "5":
+                        board[1][1] = 'X';
+                        break;
+                case "6":
+                        board[1][2] = 'X';
+                        break;
+                case "7":
+                         board[2][0] = 'X';
+                        break;
+                case "8":
+                        board[2][1] = 'X';
+                        break;
+                case "9":
+                        board[2][2] = 'X';
+                        break;
+                default:
+                        System.out.println(":(*)");
+                }
+
+                showBoard(board);
+                }
+        private static void showBoard(char[][] board) {
+                System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+                System.out.println("-+-+-");
+                System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+                System.out.println("-+-+-");
+                System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+
+
 		}
            }
